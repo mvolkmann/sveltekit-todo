9c6e056b0fa7582cacb1b374d655d3c50c8012eb
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-03-26 17:44:35 -0500

    added global.css and routes

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { page } from '$app/stores';
+  $: path = $page.path;
+
+  const routes = [
+    { name: 'About', url: '/about' },
+    { name: 'Todos', url: '/' }
+  ];
+</script>
+
+<nav>
+  {#each routes as { name, url }}
+    <a class:current={url === path} href={url}>{name}</a>
+  {/each}
+</nav>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  a {
+    color: var(--primary-color);
+    margin-right: 1rem;
+    text-decoration: none;
+  }
+
+  a.current {
+    font-weight: bold;
+  }
+
+  main {
+    padding: 1rem;
+  }
+
+  nav {
+    background-color: var(--secondary-color);
+    padding: 1rem;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,62 +1,73 @@
 <script>
-	import Todo from '../lib/Todo.svelte';
+  import Todo from '../lib/Todo.svelte';
 
-	let lastId = 0;
-	const createTodo = (text, done = false) => ({ id: ++lastId, text, done });
+  let lastId = 0;
+  const createTodo = (text, done = false) => ({ id: ++lastId, text, done });
 
-	let todoText = '';
-	let todos = [createTodo('learn Svelte', true), createTodo('build a Svelte app')];
+  let todoText = '';
+  let todos = [
+    createTodo('learn Svelte', true),
+    createTodo('build a Svelte app')
+  ];
 
-	$: uncompletedCount = todos.filter((t) => !t.done).length;
-	$: status = `${uncompletedCount} of ${todos.length} remaining`;
+  $: uncompletedCount = todos.filter((t) => !t.done).length;
+  $: status = `${uncompletedCount} of ${todos.length} remaining`;
 
-	function addTodo() {
-		todos = todos.concat(createTodo(todoText));
-		todoText = '';
-	}
+  function addTodo() {
+    todos = todos.concat(createTodo(todoText));
+    todoText = '';
+  }
 
-	const archiveCompleted = () => (todos = todos.filter((t) => !t.done));
+  const archiveCompleted = () => (todos = todos.filter((t) => !t.done));
 
-	const deleteTodo = (todoId) => (todos = todos.filter((t) => t.id !== todoId));
+  const deleteTodo = (todoId) => (todos = todos.filter((t) => t.id !== todoId));
 
-	function toggleDone(todo) {
-		const { id } = todo;
-		todos = todos.map((t) => (t.id === id ? { ...t, done: !t.done } : t));
-	}
+  function toggleDone(todo) {
+    const { id } = todo;
+    todos = todos.map((t) => (t.id === id ? { ...t, done: !t.done } : t));
+  }
 </script>
 
 <div>
-	<h2>To Do List</h2>
-	<div>
-		{status}
-		<button on:click={archiveCompleted}>Archive Completed</button>
-	</div>
-	<form on:submit|preventDefault>
-		<input
-			type="text"
-			size="30"
-			autofocus
-			placeholder="enter new todo here"
-			bind:value={todoText}
-		/>
-		<button disabled={!todoText} on:click={addTodo}> Add </button>
-	</form>
-	<ul class="unstyled">
-		{#each todos as todo}
-			<Todo {todo} on:delete={() => deleteTodo(todo.id)} on:toggleDone={() => toggleDone(todo)} />
-		{/each}
-	</ul>
+  <h2>To Do List</h2>
+  <div>
+    {status}
+    <button on:click={archiveCompleted}>Archive Completed</button>
+  </div>
+  <form on:submit|preventDefault>
+    <input
+      type="text"
+      size="30"
+      autofocus
+      placeholder="enter new todo here"
+      bind:value={todoText}
+    />
+    <button disabled={!todoText} on:click={addTodo}> Add </button>
+  </form>
+  <ul class="unstyled">
+    {#each todos as todo}
+      <Todo
+        {todo}
+        on:delete={() => deleteTodo(todo.id)}
+        on:toggleDone={() => toggleDone(todo)}
+      />
+    {/each}
+  </ul>
 </div>
 
 <style>
-	button {
-		margin-left: 10px;
-	}
+  button {
+    margin-left: 10px;
+  }
 
-	/* This removes bullets from a bulleted list. */
-	ul.unstyled {
-		list-style: none;
-		margin-left: 0;
-		padding-left: 0;
-	}
+  h2 {
+    margin-top: 0;
+  }
+
+  /* This removes bullets from a bulleted list. */
+  ul.unstyled {
+    list-style: none;
+    margin-left: 0;
+    padding-left: 0;
+  }
 </style>
